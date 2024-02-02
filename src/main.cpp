cfb9c743411633df7fa0c071e6a63d9bddcdf462
Hajime Sato <89377758+hajimesat0@users.noreply.github.com>
2024-02-02 17:55:40 +0900

    LED点灯500ms blink

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
 #include <Arduino.h>
 
+const int LED_PIN = 33;
+
 void setup() {
   printf("setup funtion\n");
+
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
   printf("loop function\n");
+
+digitalWrite(LED_PIN, HIGH);
+delay(500);
+digitalWrite(LED_PIN, LOW);
+delay(500);
+
 }
 
