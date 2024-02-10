70f5dfe7941bab52c78f85ec9ad239e5f57015f6
Hajime Sato <89377758+hajimesat0@users.noreply.github.com>
2024-02-10 11:44:19 +0900

    htmlをファイルシステムで扱うこととし、websocketのメッセージをjson化

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
 #include <WiFi.h>
+#include <SPIFFS.h>
 #include <ESPAsyncWebServer.h>
 
 const char *ssid = "Haneashi-AP";
@@ -7,39 +9,25 @@ const char *password = "123456789";
 const IPAddress ip(192, 168, 0, 100);
 const IPAddress subnet(255,255,255,0);
 
-const char html[] =
-"\
-<!DOCTYPE html><html lang='ja'><head><meta charset='UTF-8'>\
-<style>input {font-size:24pt;margin:8px;width:100px;height:80px}\
-div {font-size:24pt;color:black;text-align:center;width:400px;}</style>\
-<title>羽根足 操作</title></head>\
-<body><div><p>羽根足 操作</p>\
-<form method='get'>\
-<input type='submit' name='li' value='点灯' />\
-<input type='submit' name='lo' value='消灯' />\
-</form>\
-<button id='wson'>WS点灯</button>\
-<button id='wsoff'>WS消灯</button>\
-<script>\
-let ws = new WebSocket(`ws://${window.location.hostname}/ws`);\
-ws.onopen = function() {console.log('ws open')};\
-ws.onclose = function() {console.log('ws close')};\
-ws.onmessage = function(event) {console.log('ws message', event.data)};\
-let buttons = document.querySelectorAll('button');\
-buttons.forEach(function(button) {\
-  button.addEventListener('touchstart', function() {\
-    ws.send(button.id);\
-  });\
-});\
-</script>\
-</div></body></html>\
-";
-
 AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
 
 const int LED_PIN = 33;
 const int BUTTON_PIN = 35;
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo *)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT)
+  {
+    data[len] = 0;
+    printf((char *)data); // Add this line
+    DynamicJsonDocument doc(1024);
+    deserializeJson(doc, (char *)data);
+    String command = doc["command"].as<String>();
+    printf(doc["command"]); // Add this line
+
+  }
+}
 
 void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
   switch( type ) {
@@ -56,7 +44,8 @@ void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventT
       break;
     
     case WS_EVT_DATA:
-      printf("ws[%s][%u] recv event(%u): %s\n", server->url(), client->id(), *((uint16_t*)arg), (char*)data);
+      // printf("ws[%s][%u] recv event(%u): %s\n", server->url(), client->id(), *((uint16_t*)arg), (char*)data);
+      handleWebSocketMessage(arg, data, len);
       break;
     
     default:
@@ -67,6 +56,11 @@ void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventT
 
 void setup() {
   printf("setup funtion\n");
+
+  if( !SPIFFS.begin(true) ) {
+    printf("SPIFFS Mount Failed\n");
+    return;
+  }
 
   WiFi.mode(WIFI_AP_STA);
   WiFi.softAP(ssid, password);
@@ -85,7 +79,7 @@ void setup() {
   server.on(
     "/", HTTP_GET,
     [](AsyncWebServerRequest *request) {
-      request->send(200, "text/html", html);
+      request->send( SPIFFS, "/index.html" );
     }
   );
 
