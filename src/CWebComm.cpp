5d2d946778decdc7c63616c208c6b5f2c3c3fd17
Hajime Sato <89377758+hajimesat0@users.noreply.github.com>
2024-03-09 08:37:21 +0900

    不要コメントを削除

--- src/CWebComm.cpp
+++ src/CWebComm.cpp
@@ -55,27 +55,27 @@ void CWebComm::Loop()
 
 void CWebComm::OnWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len)
 {
-  switch( type ) {
-    case WS_EVT_CONNECT:
-    //   printf("ws[%s][%u] connect\n", server->url(), client->id());
-      break;
-    
-    case WS_EVT_DISCONNECT:
-    //   printf("ws[%s][%u] disconnect\n", server->url(), client->id());
-      break;
-    
-    case WS_EVT_ERROR:
-      printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t*)arg), (char*)data);
-      break;
-    
-    case WS_EVT_DATA:
-      handleWebSocketMessage(arg, data, len);
-      break;
-    
-    default:
-      printf("ws[%s][%u] default(%u): %s\n", server->url(), client->id(), *((uint16_t*)arg), (char*)data);
-      break;
-  }
+    switch( type ) {
+        case WS_EVT_CONNECT:
+        //   printf("ws[%s][%u] connect\n", server->url(), client->id());
+          break;
+        
+        case WS_EVT_DISCONNECT:
+        //   printf("ws[%s][%u] disconnect\n", server->url(), client->id());
+          break;
+        
+        case WS_EVT_ERROR:
+          printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t*)arg), (char*)data);
+          break;
+        
+        case WS_EVT_DATA:
+          handleWebSocketMessage(arg, data, len);
+          break;
+        
+        default:
+          printf("ws[%s][%u] default(%u): %s\n", server->url(), client->id(), *((uint16_t*)arg), (char*)data);
+          break;
+      }
 }
 
 
@@ -108,36 +108,7 @@ void CWebComm::handleWebSocketMessage(void *arg, uint8_t *data, size_t len)
         } else if( command=="ReqReset") {
             RequestListener->OnReset();
         }
-
-    // if(command == "ReqLightOn") {
-
-    //   // 左側ボタン計測開始
-    //   gIsLightOnLeft = true;
-
-    //   // LED on 
-    //   digitalWrite(LED_PIN_LEFT, HIGH);
-
-    //   // 現在時刻記録
-    //   gLightOnTime = millis();  // 現在時刻記録
-
-    //   // 割り込み有効化
-    //   attachInterrupt(BUTTON_PIN_LEFT, buttonPushedLeft, FALLING);
-    // }
-    // if(command == "bt_right") {
-    //   // 右側ボタン計測開始
-    //   gIsLightOnRight = true;
-
-    //   // LED on 
-    //   digitalWrite(LED_PIN_RIGHT, HIGH);
-
-    //   // 現在時刻記録
-    //   gLightOnTime = millis();  // 現在時刻記録
-
-    //   // 割り込み有効化
-    //   attachInterrupt(BUTTON_PIN_RIGHT, buttonPushedRight, FALLING);
-    // }
-
-  }
+    }
 }
 
 
